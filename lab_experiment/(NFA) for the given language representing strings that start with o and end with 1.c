3c22c7bbd905e87d96f02130780cb873f8730f7a
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2026-07-31 11:52:02 +0530

    Create (NFA) for the given language representing strings that start with o and end with 1.c

---
+++ lab_experiment/(NFA) for the given language representing strings that start with o and end with 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int len;
+    printf("Enter a binary string: ");
+    scanf("%s", str);
+    len = strlen(str);
+    if (str[0] == '0' && str[len - 1] == '1')
+        printf("String Accepted\n");
+    else
+        printf("String Rejected\n");
+    return 0;
+}
